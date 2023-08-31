7b74d9768e2d82317e983c33b1a5d480a5d18565
Ramon Estadella Serra <restadellaserra@gmail.com>
2023-08-31 12:48:33 +0200

    add commit

---
+++ maquetación-web/assets-proyecto/js/map.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+
+    document.querySelector("#load-iframe-map").innerHTML = `
+        <iframe  class="contact__iframe" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" loading="lazy" referrerpolicy="no-referrer-when-downgrade" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2977.4397985149562!2d1.257682876138816!3d41.732605771258044!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12a42647dffd45ef%3A0x8d1c86f2835fcc97!2s25211%20Sed%C3%B3%2C%20Lleida!5e0!3m2!1sca!2ses!4v1692283978110!5m2!1sca!2ses"></iframe>
+    `;
+});
